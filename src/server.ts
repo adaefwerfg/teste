3e31985e4baf96937fb0c8e476f07811239f5fba
Luiz Togni <contato.togni@gmail.com>
2024-08-22 16:28:25 -0300

    Port default

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import { env } from './env';
 
 app
-	.listen({ port: env.PORT })
+	.listen({ port: env.PORT || 4000 })
 	.then(() => {
 		console.log(' 🚀 Server is running!');
 		console.log(`http://localhost:${env.PORT}`);
